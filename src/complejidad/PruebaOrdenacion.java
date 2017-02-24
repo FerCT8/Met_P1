0d5f4680fc0a6439ae2d7717c6afde1bcbcf010e
Fernando <Fernando@LENOVO>
2017-02-24 17:23:54 +0100

    Merge origin/master
    Conflicts:
    	.gitignore

--- src/complejidad/PruebaOrdenacion.java
+++ src/complejidad/PruebaOrdenacion.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package complejidad.p1._metprog;
+package complejidad;
 
 import utilidades.*;
 
